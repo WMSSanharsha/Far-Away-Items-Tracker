99188dd67e2d31bf89f79cb4ad609864f37f53cc
Sakila Sanharsha <sakila.sanharsha2k19@gmail.com>
2024-09-28 22:25:16 +0530

    start of Far Away webapp

---
+++ src/App.js
@@ -0,0 +1,12 @@
+export default function App() {
+  return <div>hello</div>;
+}
+
+function Logo() {
+  return <h1>🌴Far Away 💼</h1>;
+}
+function Form() {
+  return <div className="add-form"></div>;
+}
+function PackingList() {}
+function Stats() {}
